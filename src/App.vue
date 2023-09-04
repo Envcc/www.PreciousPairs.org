9b66c5f727fc595622d46a953acca0603cce6b77
NVMelectric Innovations LLC <>
2023-09-03 21:45:34 -0700

    Jmoff
    complete but missing something in APP.VUE

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Nav-bar />
+    <router-view />
+    <Foot-er />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Nav-bar.vue';
+import Footer from './components/Foot-er.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Nav-bar,
+    Foot-er
+  }
+};
+</script>
+
